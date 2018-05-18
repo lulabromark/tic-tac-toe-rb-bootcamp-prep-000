8c9a4c80b9fd1d941a8ad12542ab001cf891d552
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-18 23:21:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,10 +75,8 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == 'X' && position_2 == 'X' && position_3 == 'X'
-      puts "Congratualtions X!"
       return win_combination
     elsif position_1 == 'O' && position_2 == 'O' && position_3 == 'O'
-      puts "Congratualtions O!"
       return win_combination
     end
   end
